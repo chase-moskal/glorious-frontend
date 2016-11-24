f4b9617000b1e6113b6d49153aeb6236789b790c
Chase Moskal <chasemoskal@gmail.com>
2016-11-23 20:59:39 -0800

    Initial commit of glory

---
+++ readme.md
@@ -0,0 +1,8 @@
+
+Glorious Frontend
+================
+
+  - TypeScript 2.1
+  - React
+  - Browserify
+  - Sass
---
+++ src/components/App.tsx
@@ -0,0 +1,24 @@
+
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+import Counter from "./Counter";
+
+export interface AppProps {}
+export interface AppState {}
+
+/**
+ * Root application component.
+ */
+export default class App extends React.Component<AppProps, AppState> {
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Glorious Frontend</h1>
+        <hr/>
+        <Counter start={0}/>
+      </div>
+    )
+  }
+}
